a1c41eb7a169c927dbc1f315c4da5218bf6a3cb6
asraful-islam <sourob2356@gmail.com>
2025-01-28 13:22:43 +0600

    add DaisyUI plugin and update App component with new styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import { useState } from 'react'
+import './App.css'
 
-const App = () => {
+function App() {
+
   return (
-    <div>
-       <h1>hello i am from app file</h1>
-    </div>
+    <>
+      <div className="mockup-phone border-primary max-h-150">
+          <div className="mockup-phone-camera"></div>
+          <div className="mockup-phone-display relative h-140">
+              <h1 className='absolute left-[36.5%] text-3xl text-white top-10'>Asraful </h1>
+              <img alt="wallpaper" className=' ' src="https://www.iclarified.com/images/news/94911/453966/453966.jpg" />
+          </div>
+      </div>
+    </>
   )
 }
 
